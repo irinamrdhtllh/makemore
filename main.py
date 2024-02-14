93b6a8410bb945361e27eedda7e11e7d3e0b046e
Irina M. S <irinamardhatillah@gmail.com>
2024-02-15 02:51:30 +0700

    Count bigrams in a 2D torch tensor

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
+import torch
+import matplotlib.pyplot as plt
+
+
 words = open("names.txt", "r").read().splitlines()
 
-count_bigram = {}
-for word in words:
-    chs = ["<S>"] + list(word) + ["<E>"]
-    for ch1, ch2 in zip(chs, chs[1:]):
-        bigram = (ch1, ch2)
-        count_bigram[bigram] = count_bigram.get(bigram, 0) + 1
+n_bigrams = torch.zeros((27, 27), dtype=torch.int32)
 
-print(count_bigram)
+chars = sorted(list(set("".join(words))))
+str_to_int = {s: i + 1 for i, s in enumerate(chars)}
+str_to_int["."] = 0
+int_to_str = {i: s for s, i in str_to_int.items()}
+
+for word in words:
+    chs = ["."] + list(word) + ["."]
+    for ch1, ch2 in zip(chs, chs[1:]):
+        i = str_to_int[ch1]
+        j = str_to_int[ch2]
+        n_bigrams[i, j] += 1
+
+plt.figure(figsize=(27, 27))
+plt.imshow(n_bigrams, cmap="Blues")
+for i in range(n_bigrams.shape[0]):
+    for j in range(n_bigrams.shape[1]):
+        ch_to_str = int_to_str[i] + int_to_str[j]
+        plt.text(j, i, ch_to_str, ha="center", va="bottom", color="gray")
+        plt.text(j, i, n_bigrams[i, j].item(), ha="center", va="top", color="gray")
+plt.axis("off")
+plt.show()
