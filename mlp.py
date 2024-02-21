2b0ca83a7dd9bfc5fb78c3e6dfbb23f32c5b08a2
Irina M. S <irinamardhatillah@gmail.com>
2024-02-21 22:50:00 +0700

    Implement batch normalization

--- mlp.py
+++ mlp.py
@@ -26,6 +26,27 @@ def build_dataset(words):
     return X, y
 
 
+@torch.no_grad()
+def calculate_loss(mode):
+    X, y = {
+        "train": (X_train, y_train),
+        "dev": (X_dev, y_dev),
+        "test": (X_test, y_test),
+    }[mode]
+
+    embed = C[X]
+    embed_concat = embed.view(embed.shape[0], -1)
+    h_preact = embed_concat @ w1 + b1
+    h_preact = (
+        batch_norm_gain * (h_preact - mean_running) / std_running + batch_norm_bias
+    )
+    h = torch.tanh(h_preact)
+    logits = h @ w2 + b2
+    loss = F.cross_entropy(logits, y)
+
+    print(f"{mode}, loss: {loss}")
+
+
 if __name__ == "__main__":
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -48,7 +69,7 @@ if __name__ == "__main__":
     X_test, y_test = build_dataset(words[n2:])
 
     # Look-up table
-    lookup_table = torch.randn((vocab_size, n_embed)).to(device=device)
+    C = torch.randn((vocab_size, n_embed)).to(device=device)
 
     # Weights and biases
     g = torch.Generator().manual_seed(32)
@@ -58,7 +79,14 @@ if __name__ == "__main__":
     b1 = torch.randn(n_hidden, generator=g).to(device=device) * 0.01
     w2 = torch.randn((n_hidden, vocab_size), generator=g).to(device=device) * 0.01
     b2 = torch.randn(vocab_size, generator=g).to(device=device) * 0.01
-    parameters = [lookup_table, w1, b1, w2, b2]
+
+    # Batch normalization parameters
+    batch_norm_gain = torch.ones((1, n_hidden)).to(device=device)
+    batch_norm_bias = torch.zeros((1, n_hidden)).to(device=device)
+    mean_running = torch.zeros((1, n_hidden)).to(device=device)
+    std_running = torch.ones((1, n_hidden)).to(device=device)
+
+    parameters = [C, w1, b1, w2, b2, batch_norm_gain, batch_norm_bias]
 
     for p in parameters:
         p.requires_grad = True
@@ -69,9 +97,17 @@ if __name__ == "__main__":
         X_batch, y_batch = X_train[ix], y_train[ix]
 
         # Forward pass
-        embed = lookup_table[X_batch]  # Embed the characters into vectors
+        embed = C[X_batch]  # Embed the characters into vectors
         embed = embed.view(embed.shape[0], -1)  # Concatenante the vectors
         h_preact = embed @ w1 + b1  # Pre-activated hidden layer
+        mean = h_preact.mean(0, keepdim=True)
+        std = h_preact.std(0, keepdim=True)
+        h_preact = batch_norm_gain * (h_preact - mean) / std + batch_norm_bias
+
+        with torch.no_grad():
+            mean_running = 0.999 * mean_running + 0.001 * mean
+            std_running = 0.999 * std_running + 0.001 * std
+
         h = torch.tanh(h_preact)  # Hidden layer
         logits = h @ w2 + b2  # Output layer
         loss = F.cross_entropy(logits, y_batch)  # Loss function
@@ -87,17 +123,12 @@ if __name__ == "__main__":
             p.data += -lr * p.grad
 
         # Track the training process
-        if i % 1000 == 0:
+        if i % 10000 == 0:
             print(f"iter: {i}, loss: {loss}")
 
     # Evaluate the model
-    embed = lookup_table[X_dev]
-    embed = embed.view(embed.shape[0], -1)
-    h_preact = embed @ w1 + b1
-    h = torch.tanh(h_preact)
-    logits = h @ w2 + b2
-    loss = F.cross_entropy(logits, y_dev)
-    print(f"eval loss: {loss}")
+    calculate_loss(mode="train")
+    calculate_loss(mode="dev")
 
     # Sample from the model
     g = torch.Generator(device=device).manual_seed(42)
@@ -105,8 +136,14 @@ if __name__ == "__main__":
         output = []
         context = [0] * block_size
         while True:
-            embed = lookup_table[torch.tensor([context])]
-            h = torch.tanh(embed.view(1, -1) @ w1 + b1)
+            embed = C[torch.tensor([context])]
+            embed = embed.view(embed.shape[0], -1)
+            h_preact = embed @ w1 + b1
+            h_preact = (
+                batch_norm_gain * (h_preact - mean_running) / std_running
+                + batch_norm_bias
+            )
+            h = torch.tanh(h_preact)
             logits = h @ w2 + b2
             probs = F.softmax(logits, dim=1)
             target = torch.multinomial(probs, num_samples=1, generator=g).item()
