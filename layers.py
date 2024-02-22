8f5f85d05b747b3980255a1ca00844a6a96bd883
Irina M. S <irinamardhatillah@gmail.com>
2024-02-22 19:31:59 +0700

    Create layer objects

---
+++ layers.py
@@ -0,0 +1,69 @@
+import torch
+import torch.nn.functional as F
+
+
+class Linear:
+    def __init__(self, fan_in, fan_out, generator, bias=True, device=None):
+        self.weight = torch.randn((fan_in, fan_out), generator=generator, device=device)
+        self.bias = torch.zeros(fan_out, device=device) if bias else None
+
+        self.weight = self.weight / fan_in**0.5
+
+    def __call__(self, x):
+        self.out = x @ self.weight
+        if self.bias is not None:
+            self.out += self.bias
+        return self.out
+
+    def parameters(self):
+        return [self.weight] + ([self.bias] if self.bias is not None else [])
+
+
+class BatchNorm1D:
+    def __init__(self, dim, epsilon=1e-5, momentum=0.1, device=None):
+        self.epsilon = epsilon
+        self.momentum = momentum
+        self.training = True
+        # Parameters (trained with back propagation)
+        self.gamma = torch.ones(dim, device=device)
+        self.beta = torch.zeros(dim, device=device)
+        # Buffers (trained with a running momentum update)
+        self.running_mean = torch.zeros(dim, device=device)
+        self.running_var = torch.ones(dim, device=device)
+
+    def __call__(self, x):
+        # Forward pass
+        if self.training:
+            x_mean = x.mean(0, keepdim=True)  # Batch mean
+            x_var = x.var(0, keepdim=True, unbiased=True)  # Batch variance
+        else:
+            x_mean = self.running_mean
+            x_var = self.running_var
+        x_hat = (x - x_mean) / torch.sqrt(
+            x_var + self.epsilon
+        )  # Normalize to unit variance
+        self.out = self.gamma * x_hat + self.beta
+
+        # Update the buffers
+        if self.training:
+            with torch.no_grad():
+                self.running_mean = (
+                    1 - self.momentum
+                ) * self.running_mean + self.momentum * x_mean
+                self.running_var = (
+                    1 - self.momentum
+                ) * self.running_var + self.momentum * x_var
+
+        return self.out
+
+    def parameters(self):
+        return [self.gamma, self.beta]
+
+
+class Tanh:
+    def __call__(self, x):
+        self.out = torch.tanh(x)
+        return self.out
+
+    def parameters(self):
+        return []
